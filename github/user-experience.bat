6718a2cfe849e8131f798ec5ef51fc8a4a579772
Drift Johnson <mushroomfleet@gmail.com>
2025-03-18 16:48:10 +0000

    Create user-experience.bat

---
+++ github/user-experience.bat
@@ -0,0 +1,2 @@
+please create a "requirements.txt" and write a new "install-cpu.bat" which uses the requirements.txt to install dependencies. 
+finally create "PROJECT_NAME_HERE.bat" which will be used to test this script.
